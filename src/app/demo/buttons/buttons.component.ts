e9b76c6a726e1b5edcea42c0e8fa1021d12b0c4f
Reza Heidari <reza.heidari@amplicade.com>
2022-12-28 17:39:45 +0300

    add buttons component

---
+++ src/app/demo/buttons/buttons.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buttons',
+  template: `
+    <p>
+      buttons works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ButtonsComponent {
+
+}
