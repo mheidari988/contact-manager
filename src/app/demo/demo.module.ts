47f6b3a4b4f8b76a028e1bdcb41ff91f579bb8b2
Reza Heidari <reza.heidari@amplicade.com>
2022-12-29 10:04:07 +0300

    add contactmanager

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '../shared/material.module';
 import { FormsModule } from '@angular/forms';
-
 import { DemoRoutingModule } from './demo-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
+import { FlexboxComponent } from './flexbox/flexbox.component';
 
 @NgModule({
-  declarations: [ButtonsComponent],
-  imports: [CommonModule, DemoRoutingModule, MaterialModule, FormsModule],
+  declarations: [ButtonsComponent, FlexboxComponent],
+  imports: [
+    CommonModule,
+    DemoRoutingModule,
+    MaterialModule,
+    FormsModule,
+    FlexLayoutModule,
+  ],
 })
 export class DemoModule {}
